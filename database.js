463f55d8b88d68251f159a02f7870627a31a4e1c
Akahay-udapure <akshayudapure5@gmail.com>
2024-12-31 08:09:38 +0530

    install mongodb and connect to db

---
+++ database.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require('mongodb');
+const url = "mongodb+srv://akshayudapure:InoQ3DTwRhdKdZE0@namastenodejs.hqam4.mongodb.net/";
+const client = new MongoClient(url);
+const dbName = 'HelloWorld';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('User');
+  await collection.insertOne({firstName:"Akshay", lastName:"Saini", age:30, city:"Dehradun", phoneNumber:"9878382838"})
+  // the following code examples can be pasted here...
+  const users = await collection.find({}).toArray();
+  console.log(users,"===");
+  
+
+  return 'done.';
+}
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
